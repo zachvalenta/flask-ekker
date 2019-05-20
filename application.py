af8d163e17b98737de9d9f44c691bcf486aefd7a
zachvalenta <zjv5002@gmail.com>
2019-05-20 18:34:35 -0400

    in-mem store for form input, helper func, add to index template

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 
 from dotenv import find_dotenv, load_dotenv
@@ -6,7 +7,16 @@ from loguru import logger
 
 app = Flask(__name__)
 load_dotenv(find_dotenv())
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY").encode()
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY").encode()
+bookmarks = []
+
+
+def store_bookmark(input):
+    bookmarks.append(dict(input=input, user="zjv", date=datetime.utcnow()))
+
+
+def get_bookmarks_by(sort_key):
+    return sorted(bookmarks, key=lambda bm: bm[sort_key])
 
 
 @app.errorhandler(404)
@@ -21,14 +31,19 @@ def not_found(e):
 @app.route("/index")
 def index():
     return render_template(
-        "index.html", title="index", paragraph=["sentence one", "sentence two"]
+        "index.html",
+        title="index",
+        paragraph=["sentence one", "sentence two"],
+        marks=get_bookmarks_by("date"),
     )
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        logger.debug(request.form['dummy_form'])
-        flash(request.form['dummy_form'])
-        return redirect(url_for('index'))
+        form_input = request.form["dummy_form"]
+        logger.debug("form input -> {}".format(form_input))
+        flash(form_input)  # store flash in session obj
+        store_bookmark(form_input)
+        return redirect(url_for("index"))
     return render_template("add.html")
