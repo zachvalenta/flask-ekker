a9b76743596645cbf9b821cfc593ee24e73dcf45
zachvalenta <zjv5002@gmail.com>
2019-05-25 00:15:58 -0400

    basic models

---
+++ models.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from application import db
+
+
+class Input(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    input = db.Column(db.String(80), unique=True, nullable=False)
+    time = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "id {} time {} input {}".format(self.id, self.input, self.time)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
